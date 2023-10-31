5aaa0f70c18f6f406ca40cf6016041979f7b97f8
Mugabo <dinturner17@gmail.com>
2023-10-31 13:05:19 -0700

    If/Else

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("best")
-uppercase("Best School 98 Battery street"
+def uppercase(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+            result += uppercase_char
+        else:
+            result += char
+    print(result)
