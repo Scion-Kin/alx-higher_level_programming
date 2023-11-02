ed2fdffc49dd8593df94be69318e16bda8cc5e84
Scion-Kin <dinturner17@gmail.com>
2023-11-02 22:55:54 +0200

    Finally.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    count =  len(sys.argv) - 1
+
+    if count == 0:
+        print("0 arguments.")
+
+    if count == 1:
+        print("1 argument:\n1:", sys.argv[1])
+
+    if count > 1:
+        print("{} arguments:".format(count))
+        for i in range(1, count + 1):
+            print(f"{i}: {sys.argv[i]}")
