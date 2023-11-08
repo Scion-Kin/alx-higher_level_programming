ceb431fa161432a3ec7c4480db562d3eb53bd31a
Scion-Kin <dinturner17@gmail.com>
2023-11-08 20:40:56 +0200

    More

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    roms = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ints = [roms[i] for i in roman_string] + [0]
+    result = 0
+
+    for i in range(len(ints) - 1):
+        if ints[i] >= ints[i+1]:
+            result += ints[i]
+        else:
+            result -= ints[i]
+
+    return result
