737025390091cd46bfcefce871d82885171bea43
Scion-Kin <dinturner17@gmail.com>
2023-11-08 20:06:13 +0200

    More

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,9 @@
 
 def best_score(a_dictionary):
     j = 0
+    k = ""
     for key, value in a_dictionary.items():
         if value > j:
             j = value
-    return j
+            k = key
+    return k
