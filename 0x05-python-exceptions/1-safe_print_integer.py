4af7b747eb95af61328486bae9adb40839eed548
Scion-Kin <dinturner17@gmail.com>
2023-11-20 16:18:20 +0200

    exception handled

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
+
     try:
         print("{:d}".format(value))
 
     except TypeError:
         return False
-    
+
     else:
         return True
