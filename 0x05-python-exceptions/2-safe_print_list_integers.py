ecc83f4c24579f56c1ddc0614adb686dbc582a6c
Scion-Kin <dinturner17@gmail.com>
2023-11-20 16:44:52 +0200

    exception handled

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    count = 0
+
+    for i in range(x):
+
+        try:
+            print("{:d}".format(my_list[i]), end="")
+
+        except (TypeError, ValueError):
+            x += 1
+            continue
+
+        except IndexError:
+            raise
+
+        else:
+            count += 1
+
+    print("\n")
+    return count
