103f5421fc659cdc2f4b7c1d932abd537cf839b1
Scion-Kin <dinturner17@gmail.com>
2023-11-21 18:09:21 +0200

    Python classes

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Defining a class'''
+
+
+class Square:
+    '''If anything apart from int is passed, I raise an error'''
+
+    def __init__(here, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        here.__size = size
+
+    def area(here, size):
+        return (here.__size * here.__size)
