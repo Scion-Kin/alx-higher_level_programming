b8f55fc2ca0615651da67c30910f736de91eb4b8
Scion-Kin <dinturner17@gmail.com>
2023-11-21 16:44:08 +0200

    Python classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+class Square:
+    '''This class creates a square. This is a basic class definition,
+    but super effective to allow user to be as flexible as they like'''
+    pass
