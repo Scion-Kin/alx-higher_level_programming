6fa7356a7e25ca608c888075ead15539771cf406
Scion-Kin <dinturner17@gmail.com>
2023-11-21 20:28:59 +0200

    Python classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,52 +1,50 @@
 #!/usr/bin/python3
 '''Defining a class'''
 
-
 class Square:
     '''If anything apart from int is passed, I raise an error'''
 
-    def __init__(here, size=0, position=(0, 0)):
-
-        here.size = size
-        here.position = position
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
 
     @property
-    def size(here):
-        return here.__size
+    def size(self):
+        return self.__size
 
     @size.setter
-    def size(here, value):
+    def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
         elif value < 0:
             raise ValueError("size must be >= 0")
 
-        here.__size = value
+        self.__size = value
 
     @property
-    def position(here):
-        return here.__position
+    def position(self):
+        return self.__position
 
     @position.setter
-    def position(here, value):
+    def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
 
-        here.__position = value
+        self.__position = value
 
-    def area(here):
-        return here.__size * here.__size
+    def area(self):
+        return self.__size * self.__size
 
-    def my_print(here):
-        if here.__size == 0:
+    def my_print(self):
+        if self.__size == 0:
             print()
 
-        print(""*here.__position[1])
-        for i in range(here.__size):
-            print(" "*here.__position[0], end="")
-            print("#"*here.__size, end="")
-            print("")
+        print("" * self.__position[1])
+        for i in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size, end="")
+            print()
