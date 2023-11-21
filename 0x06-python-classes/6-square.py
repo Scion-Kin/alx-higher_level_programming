ea2f0f70090c0022e93a87989ef82c77df963825
Scion-Kin <dinturner17@gmail.com>
2023-11-21 20:50:57 +0200

    Python classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,46 +5,46 @@
 class Square:
     '''If anything apart from int is passed, I raise an error'''
 
-    def __init__(self, size=0, position=(0, 0)):
-        self.size = size
-        self.position = position
+    def __init__(here, size=0, position=(0, 0)):
+
+        here.size = size
+        here.position = position
 
     @property
-    def size(self):
-        return self.__size
+    def size(here):
+        return here.__size
 
     @size.setter
-    def size(self, value):
+    def size(here, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
         elif value < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = value
+        here.__size = value
 
     @property
-    def position(self):
-        return self.__position
+    def position(here):
+        return here.__position
 
     @position.setter
-    def position(self, value):
+    def position(here, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
 
-        self.__position = value
+        here.__position = value
 
-    def area(self):
-        return self.__size * self.__size
+    def area(here):
+        return here.__size * here.__size
 
-    def my_print(self):
-        if self.__size == 0:
+    def my_print(here):
+        if here.__size == 0:
             print()
 
-        else:
-            print("" * self.__position[1], end="")
-            for i in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+        print("\n"*here.__position[1], end="")
+        for i in range(here.__size):
+            print(" "*here.__position[0] + "#"*here.__size)
