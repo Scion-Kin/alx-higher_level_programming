eb613bc570601a0153afa98f2a741c31279c8bd9
Scion-Kin <dinturner17@gmail.com>
2023-11-29 00:03:40 +0200

    Objects

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    return "BestSchool" * magic_string.counter
-magic_string.counter = 1
+def magic_string(li=[]):
+    li += ['Best School']
+    return ", ".join(li)
