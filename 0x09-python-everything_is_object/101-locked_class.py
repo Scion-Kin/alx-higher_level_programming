0a9cb6a88caab273c8cfa18ffa3beef30d8cac28
Scion-Kin <dinturner17@gmail.com>
2023-11-29 00:21:33 +0200

    Objects

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+'''A locked class definition'''
+
 
 class LockedClass:
+    '''defines a locked class to be used only is instance is name ***first_name***'''
+
     __slots__ = 'first_name'
