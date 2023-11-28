429d20e87959d0b3f51029916354e1eb24054265
Scion-Kin <dinturner17@gmail.com>
2023-11-28 15:11:27 +0200

    More classes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -5,20 +5,17 @@
 class Rectangle:
     """Read the code"""
     def __init__(self, width=0, height=0):
-        """dodumentation pending"""
 
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """dodumentation pending"""
 
         return self.__width
 
     @width.setter
     def width(self, value):
-        """dodumentation pending"""
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -30,13 +27,11 @@ class Rectangle:
 
     @property
     def height(self):
-        """dodumentation pending"""
 
         return self.__height
 
     @height.setter
     def height(self, value):
-        """documentation pending"""
 
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
@@ -45,3 +40,22 @@ class Rectangle:
             raise ValueError("height must be >= 0")
 
         self.__height = value
+
+    def area(self):
+
+        return self.__width * self.__height
+
+    def perimeter(self):
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+
+        for i in range(self.__height):
+            print("#" * self.__width, end="")
+            print()
+
+        return ""
