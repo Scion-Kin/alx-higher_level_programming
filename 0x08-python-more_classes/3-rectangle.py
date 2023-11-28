1b842187ff0ddb98185522eaeffa397e9aa2ecbf
Scion-Kin <dinturner17@gmail.com>
2023-11-28 15:22:49 +0200

    More classes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -55,4 +55,6 @@ class Rectangle:
 
     def __str__(self):
 
+        if self.__height == 0 or self.__width == 0:
+            return ""
         return (("#") * self.__width + '\n') * self.__height
