a4537ac8f5d957bb910364dad1186a17386bc16c
Scion-Kin <dinturner17@gmail.com>
2023-12-05 00:11:22 +0200

    Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module containing class"""
+
+
+class BaseGeometry:
+    """An class with a method that raises an exception"""
+
+    def area(self):
+        """raises an exception"""
+        raise Exception(area() is not implemented)
