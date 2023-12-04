c98f8038e9cf0fc2eae7d3a4334b3e89cca3fb32
Scion-Kin <dinturner17@gmail.com>
2023-12-04 23:23:16 +0200

    Inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module containing a function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if obj is an instance of a_class, which it is or not inherited from"""
+
+    return isinstance(obj, a_class)
