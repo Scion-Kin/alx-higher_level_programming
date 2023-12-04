e2a9842ad9bcccee5af6658b93cb2314beab8895
Scion-Kin <dinturner17@gmail.com>
2023-12-04 23:31:04 +0200

    Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     """Checks if obj is inherited from a_class"""
 
-    return (issubclass(type(obj), a_class) and obj != a_class)
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
