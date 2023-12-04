ea3d081df74db01ad330eda6f489a1d55bd78d3e
Scion-Kin <dinturner17@gmail.com>
2023-12-04 23:27:44 +0200

    Inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module containing a function"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if obj is inherited from a_class"""
+
+    return issubclass(type(obj), a_class) and obj != a_class
