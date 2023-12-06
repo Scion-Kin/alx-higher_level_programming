4e5b2f60788c81900546c5ecdf766e919405518b
Scion-Kin <dinturner17@gmail.com>
2023-12-06 23:23:21 +0200

    Input-Output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-'''This is a script generated documentation'''
+'''This module deals with handling JSON data'''
+
+
+def load_from_json_file(filename):
+    '''This function reads from JSON file and decodes the data'''
+
+    import json
+
+    with open(filename) as file:
+        data = json.load(file)
+
+    return data
