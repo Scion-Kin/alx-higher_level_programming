286ceb5c20ef8fc2a958d39cadd4270000fb882b
Scion-Kin <dinturner17@gmail.com>
2023-12-06 14:53:00 +0200

    Inheritance

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+'''This is a script generated documentation'''
+
+
+def read_file(filename=""):
+
+    import os
+
+    with open(filename, mode="r", encoding="UTF-8") as text:
+        for i in text:
+            print(i, end="")
