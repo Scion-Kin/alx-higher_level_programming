2353d34d0170485c1f1e710a43d9aa9ffd66677c
Scion-Kin <dinturner17@gmail.com>
2023-12-06 22:59:55 +0200

    Input-Output

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-'''This is a script generated documentation'''
+'''This is a module containing a function for handling JSON data'''
+
+
+def from_json_string(my_str):
+    '''This function eturns an object (Python data structure)
+    represented by a JSON string'''
+
+    import json
+
+    data = json.loads(my_str)
+
+    return data
