6ae4a100ddd6c870630b256c97c4b866364245b2
Scion-Kin <dinturner17@gmail.com>
2023-12-06 22:36:58 +0200

    Input-Output

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 '''This is a script generated documentation'''
+
+
+def to_json_string(my_obj):
+    '''Returns a JSON representation of a passed object'''
+
+    import json
+
+    data = json.dumps(my_obj)
+
+    return data
