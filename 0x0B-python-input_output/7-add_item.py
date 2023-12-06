849c7e96de4c46e76adfee82d5f13cb14758c28d
Scion-Kin <dinturner17@gmail.com>
2023-12-07 00:13:42 +0200

    Input-Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
-'''This is a script generated documentation'''
+'''This script handles JSON data'''
+
+
+if __name__ == "__main__":
+    '''This writes arguments of this script in a file'''
+
+    import sys
+
+    writer = __import__('5-save_to_json_file').save_to_json_file
+    opener = __import__('6-load_from_json_file').load_from_json_file
+
+    argc = len(sys.argv)
+    li = []
+    word = ""
+
+    for i in range(1, argc):
+        word += sys.argv[i] + " "
+
+    li += word.split()
+    print(word)
+
+    writer(li, 'add_item.json')
