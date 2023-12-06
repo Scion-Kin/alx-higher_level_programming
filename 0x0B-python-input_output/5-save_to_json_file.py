32bfe3937f49ba60c9720e8a818af426648bb8df
Scion-Kin <dinturner17@gmail.com>
2023-12-06 23:10:48 +0200

    Input-Output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-'''This is a script generated documentation'''
+'''This module deals with handling JSON data'''
+
+
+def save_to_json_file(my_obj, filename):
+    '''This function writes the data in "my_obj" to the
+    file passed as "filename"'''
+
+    import json
+
+    with open(filename, mode="w") as file:
+        json.dump(my_obj, file)
