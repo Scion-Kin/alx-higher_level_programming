e78af292e1c5e67818abe6c85652e70398ad4798
Scion-Kin <dinturner17@gmail.com>
2023-12-06 15:12:13 +0200

    Input-Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,6 @@
 
 
 def read_file(filename=""):
-    """Prints the content of a passed file"""
-
-    with open(filename, encoding="utf-8") as text:
-        print(text.read(), end="")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
