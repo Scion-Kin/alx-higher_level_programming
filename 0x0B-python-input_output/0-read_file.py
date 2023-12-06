a047f1b4099810f8cc4e3a4edeea7bb2c8d951f6
Scion-Kin <dinturner17@gmail.com>
2023-12-06 15:08:30 +0200

    Input-Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
-'''A module containingf a function for reading files'''
+"""A module containing a function for reading files"""
 
 
 def read_file(filename=""):
-    '''Prints the content of a passed file'''
+    """Prints the content of a passed file"""
 
-    with open(filename, mode="r", encoding="UTF-8") as text:
-        print(text.read(), end="")
+    with open(filename, mode="r", encoding="utf-8") as text:
+        for i in text:
+            print(i, end="")
