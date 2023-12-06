faf71692acef2e6a3fbca6d0843aa8db636c7bb8
Scion-Kin <dinturner17@gmail.com>
2023-12-07 00:53:41 +0200

    Input-Output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-'''This is a script generated documentation'''
+'''This module contains a class definition'''
+
+
+class Student:
+    '''Class that defines a student's properties and methods'''
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
