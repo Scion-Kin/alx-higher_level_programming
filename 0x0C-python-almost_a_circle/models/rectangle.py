5b13cb725bf8c5ce2000c9ac4bce6295fb8f9909
Scion-Kin <dinturner17@gmail.com>
2023-12-11 20:37:26 +0200

    Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,11 +9,11 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         '''Documentation pending'''
 
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
 
     @property
     def width(self):
