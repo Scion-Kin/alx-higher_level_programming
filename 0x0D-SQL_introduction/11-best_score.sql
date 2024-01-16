0cdb4a9340e9ed250cb81e4d45ddec3417fb5e42
Scion-Kin <dinturner17@gmail.com>
2024-01-16 23:19:47 +0200

    Task completed

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all data from score and name column where id is >= 10
-SELECT `score`, `name` FROM `second_table` WHERE `id` >= 10 ORDER BY `score` DESC
+SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC
