139f2488d8c31b482de1ae3054594024ee58ea2b
Scion-Kin <dinturner17@gmail.com>
2024-01-17 00:05:07 +0200

    Task completed

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists records with a name
-SELECT `score`, `name` FROM `second_table` WHERE `name` IS NOT "" ORDER BY `score` DESC
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC
