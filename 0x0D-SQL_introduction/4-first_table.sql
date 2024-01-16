5f49c7ba295a63eea75abddb01ad03888c65cb77
Scion-Kin <dinturner17@gmail.com>
2024-01-16 22:31:50 +0200

    Task completed

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- creates a new table in the current or given database
-CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256))
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256))
