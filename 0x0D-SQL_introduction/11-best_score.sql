56e7b741693dfa81932b80180f71daefaea7edb7
Scion-Kin <dinturner17@gmail.com>
2024-01-16 23:25:04 +0200

    Task completed

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
--- lists all data from score and name column where id is >= 10
+-- lists all data from score and name column where score is >= 10
 SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- updates bob's score
+UPDATE `score` VALUES (10) FROM `second_table` WHERE `name` = 'Bob'
