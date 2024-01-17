3011ad2d9b07ce5703d2163bc0b585dbfbace4fe
Scion-Kin <dinturner17@gmail.com>
2024-01-17 22:32:42 +0200

    Task completed

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates a user with all priviledges
-CREATE USER `user_0d_1` IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO user_0d_1;
