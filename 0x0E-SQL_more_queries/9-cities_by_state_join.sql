bdf79a8bb478f6f6a46ff96f0208281eac8d682e
Scion-Kin <dinturner17@gmail.com>
2024-01-18 00:05:03 +0200

    Task completed

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities in a table
-SELECT cities.id, cities.name, states.name WHERE cities.state_id = states.id;
+SELECT cities.id, cities.name, states.name FROM cities WHERE cities.state_id = states.id;
