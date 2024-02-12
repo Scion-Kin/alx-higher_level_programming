8a1fc2ba477f30bdb748eabc69cea1d3bc1f8193
Scion-Kin <dinturner17@gmail.com>
2024-02-12 14:40:04 +0200

    Task completed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+if (isNaN(process.argv[2]) || !process.argv[2] || process.argv.length < 4) {
+  console.log('0');
+} else {
+  let i = 2;
+  const arr = [];
+
+  while (process.argv[i]) {
+    arr.push(Number(process.argv[i]));
+    i++;
+  }
+  console.log(Math.max(...arr));
+}
