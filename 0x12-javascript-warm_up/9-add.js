a4ee319a8da3426ae81444a5c766a5daed00db62
Scion-Kin <dinturner17@gmail.com>
2024-02-12 14:02:30 +0200

    Task completed

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,11 +5,3 @@ function add (a, b) {
 }
 
 add(process.argv[2], process.argv[3]);
-
-/* Write a script that prints the addition of 2 integers
-
-    The first argument is the first integer
-    The second argument is the second integer
-    You have to define a function with this prototype:
-    You must use console.log(...) to print all output
-    You are not allowed to use var */
