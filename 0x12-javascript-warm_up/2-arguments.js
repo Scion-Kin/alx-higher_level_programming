b2b04aefb875555170edf065b6ed39a44c7d9f99
Scion-Kin <dinturner17@gmail.com>
2024-02-12 12:38:37 +0200

    Task completed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
-
-if (!argv[2]) {
+if (!process.argv.length === 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
-} else if (argv.length > 3) {
+} else {
   console.log('Arguments found');
 }
