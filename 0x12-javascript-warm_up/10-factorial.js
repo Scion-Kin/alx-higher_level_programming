2616185e796b818332df09caf2c26b388de6ea4f
Scion-Kin <dinturner17@gmail.com>
2024-02-12 14:21:19 +0200

    Task completed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+function factorial (num) {
+  if (num === 0) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
+}
+
+if (isNaN(process.argv[2]) || !process.argv[2]) {
+  console.log('1');
+} else {
+  const num = factorial(parseInt(process.argv[2]));
+  console.log(num);
+}
