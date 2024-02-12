cc5c09ac6f94dbadc2c32ccdad1ccf1c0ad43843
Scion-Kin <dinturner17@gmail.com>
2024-02-12 13:36:53 +0200

    Task completed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+if (isNaN(process.argv[2]) || !process.argv[2]) {
+  console.log('Missing size');
+} else {
+  let square = '';
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    square += 'X';
+  }
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(square);
+  }
+}
