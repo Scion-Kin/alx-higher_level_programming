8384ab6da0346c9a010cd260ebf64f21ac0e2c7e
Scion-Kin <dinturner17@gmail.com>
2024-02-13 16:32:33 +0200

    Task completed

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,33 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let string = '';
+    for (let i = 0; i < this.width; i++) {
+      string += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(string);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2; // multiply by 2
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
