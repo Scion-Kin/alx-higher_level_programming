f48ae1dc0ca4cd62dca13a8f088d0fa337bc54e7
Scion-Kin <dinturner17@gmail.com>
2024-02-13 22:16:53 +0200

    Task completed

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 const { list } = require('./100-data');
 
-const newLi = list.map((x) => x * 2);
+const newLi = list.map((x, index, array) => {
+  if (index === 0) {
+    return (0);
+  }
+  return (array[index - 1] * x);
+});
 
 console.log(list);
 console.log(newLi);
