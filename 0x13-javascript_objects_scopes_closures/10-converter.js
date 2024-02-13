f0e6fd709733f9fd11830adca5a17f1373edfc93
Scion-Kin <dinturner17@gmail.com>
2024-02-13 18:26:19 +0200

    Task completed

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+exports.converter = function (base) {
+  return function convertToBase (num) {
+    if (num < base) {
+      // Base case: when the number is less than the base, return it as a string
+      return num.toString(base);
+    } else {
+      // Recursive case: divide the number by the base and continue converting
+      return convertToBase(Math.floor(num / base)) + convertToBase(num % base);
+    }
+  };
+};
