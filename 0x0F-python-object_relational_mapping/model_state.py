f7755cd7231f85aa79e3ca0c5db939990b6489f2
Scion-Kin <dinturner17@gmail.com>
2024-02-18 23:17:22 +0200

    Task completed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 '''This module defines a class named states for SQLAlchemy'''
-from sqlalchemy import Integer, String, Column
+from sqlalchemy import Integer, String, Column, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+meta = MetaData()
+Base = declarative_base(meta)
 
 
 class State(Base):
