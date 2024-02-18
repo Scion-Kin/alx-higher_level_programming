4795076e3ef21781b835728aed95287b6fb48792
Scion-Kin <dinturner17@gmail.com>
2024-02-18 21:08:55 +0200

    Task completed

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE BINARY "{}"'
-                .format(sys.argv[4]))
+    cur.execute('SELECT * FROM states WHERE name LIKE BINARY %s',
+                (sys.argv[4],))
 
     rows = cur.fetchall()
 
