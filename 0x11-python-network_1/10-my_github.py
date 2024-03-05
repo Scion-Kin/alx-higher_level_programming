8427a8e7f1c012bb9493a061fc920fe9a07eaa2e
Scion-Kin <dinturner17@gmail.com>
2024-03-05 20:35:01 +0200

    Task completed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+''' This scripts handles JSON responses '''
+
+if __name__ == '__main__':
+
+    import sys
+    import requests
+    from requests.auth import HTTPBasicAuth
+
+    authentication = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+
+    r = requests.get('https://api.github.com/user', auth=authentication)
+
+    print(r.json().get('id'))
