39761f802b984c1e284f4c644e9290e9643b9b46
Scion-Kin <dinturner17@gmail.com>
2024-03-05 19:06:51 +0200

    Task completed

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+''' This script makes a request with custom parameters '''
+
+if __name__ == '__main__':
+
+    import sys
+    import requests
+
+    r = requests.get(sys.argv[1], params={'email': sys.argv[2]})
+
+    print(r.text)
