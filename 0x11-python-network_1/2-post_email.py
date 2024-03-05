25be7b55277d3470f0abda644de7353d8657e201
Scion-Kin <dinturner17@gmail.com>
2024-03-05 18:08:33 +0200

    Task completed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+''' This script makes a post request to a given site
+with an email variable  and displays the response body '''
+
+if __name__ == '__main__':
+
+    import sys
+    from urllib.request import urlopen, Request
+    from urllib import parse
+
+    email = sys.argv[2]
+    site = sys.argv[1]
+
+    data = parse.urlencode({'email': email})
+    data = data.encode('ascii')
+
+    req = Request(site, data)
+
+    with urlopen(req) as response:
+        print(response.decode('utf-8'))
