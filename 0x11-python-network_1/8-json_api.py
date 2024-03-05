351f1243eb7d96ceb7247a1b42b64744fbdfb9a7
Scion-Kin <dinturner17@gmail.com>
2024-03-05 20:04:58 +0200

    Task completed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+''' This scripts handles JSON responses '''
+
+if __name__ == '__main__':
+
+    import sys
+    import requests
+
+    q = sys.argv[1] if len(sys.argv) > 1 else ''
+
+    r = requests.get('http://0.0.0.0:5000/search_user', data={'q': q})
+
+    try:
+        if r.json() == {}:
+            print('No result')
+
+        else:
+            for key, value in r.json():
+                print('[{}] {}'.format(key, value))
+
+    except ValueError:
+        print('Not a valid JSON')
