b81d0e9ad0700fec866d89f408cb0029c29dcf53
Scion-Kin <dinturner17@gmail.com>
2024-03-05 17:09:40 +0200

    Task completed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     with urlopen(req) as data:
         data = data.read()
         print('Body response:')
-        print('    - type: ', type(data))
-        print('    - content: ', data)
-        print('    - utf8 content: ', data.decode('utf-8'))
+        print('\t- type: ', type(data))
+        print('\t- content: ', data)
+        print('\t- utf8 content: ', data.decode('utf-8'))
