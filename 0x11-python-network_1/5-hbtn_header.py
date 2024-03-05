52db5b74fa16447f8ec3d075696e01c5fc1c4ed0
Scion-Kin <dinturner17@gmail.com>
2024-03-05 18:51:55 +0200

    Task completed

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,5 +8,4 @@ if __name__ == '__main__':
 
     r = requests.get(sys.argv[1])
 
-    if r.headers['X-Request-Id']:
-        print(r.headers['X-Request-Id'])
+    print(r.headers.get('X-Request-Id'))
