8f96991a3a90fa1e1a0f0be7e162558bd781bfed
Scion-Kin <dinturner17@gmail.com>
2024-03-05 17:06:20 +0200

    Task completed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-''' This script fetches a web page '''
+''' This script fetches a web page and displays it's output '''
 
-from urllib.request import Request, urlopen
+if __name__ == '__main__':
 
-req = Request('https://alx-intranet.hbtn.io/status')
+    from urllib.request import Request, urlopen
 
-with urlopen(req) as data:
-    data = data.read()
-    print('Body response:')
-    print('    - type: ', type(data))
-    print('    - content: ', data)
-    print('    - utf8 content: ', data.decode('utf-8'))
+    req = Request('https://alx-intranet.hbtn.io/status')
+
+    with urlopen(req) as data:
+        data = data.read()
+        print('Body response:')
+        print('    - type: ', type(data))
+        print('    - content: ', data)
+        print('    - utf8 content: ', data.decode('utf-8'))
