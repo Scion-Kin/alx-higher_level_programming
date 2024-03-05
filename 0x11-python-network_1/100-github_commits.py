dd1f1b3d61a8bf3d8af55906bf761b0daef9d685
Scion-Kin <dinturner17@gmail.com>
2024-03-05 21:25:43 +0200

    Task completed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,15 +5,16 @@ if __name__ == '__main__':
 
     import sys
     import requests
-    # from requests.auth import HTTPBasicAuth
-
-    # creds = HTTPBasicAuth(sys.argv[1], sys.argv[2])
 
     req = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
         sys.argv[1], sys.argv[2]))
 
     r = req.json()
 
-    for i in range(10):
-        print('{}: {}'.format(r[i].get('sha'),
-              r[i].get('commit').get('author').get('name')))
+    try:
+        for i in range(10):
+            print('{}: {}'.format(r[i].get('sha'),
+                  r[i].get('commit').get('author').get('name')))
+
+    except IndexError:
+        pass
