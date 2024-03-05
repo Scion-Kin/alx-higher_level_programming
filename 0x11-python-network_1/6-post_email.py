03b08bad11642cee79b0f06d7241bfeffd1c9a2d
Scion-Kin <dinturner17@gmail.com>
2024-03-05 19:07:58 +0200

    Task completed

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     import sys
     import requests
 
-    r = requests.get(sys.argv[1], params={'email': sys.argv[2]})
+    r = requests.post(sys.argv[1], params={'email': sys.argv[2]})
 
     print(r.text)
