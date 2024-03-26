33c03e69731b0f4685b34239bf725dbc5a32d3ce
Scion-Kin <dinturner17@gmail.com>
2024-03-26 16:49:08 +0200

    Task completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,7 +4,6 @@ const request = require('request');
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 request.get(url, function (error, response, body) {
-
   if (error) console.log(error);
   else {
     const resJson = JSON.parse(body);
@@ -12,13 +11,13 @@ request.get(url, function (error, response, body) {
     const users = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const dict = {};
     for (const i of users) {
-        let count = 0;
-        for (const j of resJson) {
-            if (i == j.userId && j.completed == true) {
-                count++;
-            }
+      let count = 0;
+      for (const j of resJson) {
+        if (i === j.userId && j.completed === true) {
+          count++;
         }
-        dict[i] = count;
+      }
+      dict[i] = count;
     }
     console.log(dict);
   }
