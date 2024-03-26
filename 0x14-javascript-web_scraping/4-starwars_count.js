3180bd00fd5c5fed06ff779da1f122b549827c1e
Scion-Kin <dinturner17@gmail.com>
2024-03-26 16:01:45 +0200

    Task completed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,14 @@
 const request = require('request');
 
 request.get(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log(error);
-  }
-  const resJson = JSON.parse(body);
-  let count = 0;
-  for (const i of resJson.results) {
-    if (i.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      count++;
+  if (!error) {
+    const resJson = JSON.parse(body);
+    let count = 0;
+    for (const i of resJson.results) {
+      if (i.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count++;
+      }
     }
+    console.log(count);
   }
-  console.log(count);
 });
