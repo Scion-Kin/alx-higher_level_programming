e0f442d9a06c6164ee4355a4eb4b294a57cf0cb1
Scion-Kin <dinturner17@gmail.com>
2024-03-26 19:42:33 +0200

    Task completed

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,17 +4,21 @@ const request = require('request');
 
 const movieId = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request.get(movieId, function (error, response, body) {
+request(movieId, async function (error, response, body) {
   if (!error) {
     const resJson = JSON.parse(body);
     const people = resJson.characters;
     for (const i of people) {
-      request.get(i, function (error, response, body) {
-        if (!error) {
-          const personName = JSON.parse(body).name;
-          console.log(personName);
-        }
+      const requestTime = await new Promise((resolve, reject) => {
+        request(i, function (error, response, body) {
+          if (!error) {
+            resolve({ body });
+          } else {
+            reject(error);
+          }
+        });
       });
+      console.log(JSON.parse(requestTime.body).name);
     }
   }
 });
