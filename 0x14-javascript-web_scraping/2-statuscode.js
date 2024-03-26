1b8a55fdfa587972688efc9513626304b81f9ed0
Scion-Kin <dinturner17@gmail.com>
2024-03-26 15:20:04 +0200

    Task completed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,5 +6,5 @@ request.get(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   }
-  console.log('code: ', response.statusCode);
+  console.log(`code: ${response.statusCode}`);
 });
